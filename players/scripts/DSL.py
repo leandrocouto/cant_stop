531eba1add5dbab6793cb5c806124a8338c09d43
levilelis <levilelis@gmail.com>
2020-02-10 13:53:52 -0700

    removing or clauses from the DSL

--- players/scripts/DSL.py
+++ players/scripts/DSL.py
@@ -14,7 +14,8 @@ class DSL:
         self._grammar[self.start] = ['if B S', '']
         self._grammar[self.start_single] = ['if B', '']
         #self._grammar['B'] = ['B and B', 'B or B', 'F > NUMBER', 'F < NUMBER', 'F == NUMBER', 'B1']
-        self._grammar['B'] = ['B1', 'B1 and B1', 'B1 or B1']
+#         self._grammar['B'] = ['B1', 'B1 and B1', 'B1 or B1']
+        self._grammar['B'] = ['B1', 'B1 and B1']
         self._grammar['B1'] = ['DSL.isDoubles(a)', 'DSL.containsNumber(a, NUMBER )', 'DSL.actionWinsColumn(state,a)', 'DSL.hasWonColumn(state,a)', 
                                'DSL.numberPositionsProgressedThisRoundColumn(state, NUMBER ) > SMALL_NUMBER and DSL.isStopAction(a)', 'DSL.isStopAction(a)',
                                'DSL.numberPositionsConquered(state, NUMBER ) > SMALL_NUMBER and DSL.containsNumber(a, NUMBER )']
