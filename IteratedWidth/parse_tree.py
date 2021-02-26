e0b2881373a5fd248d00c803b651f7946b572f51
Leandro <leandrocoutom@gmail.com>
2021-02-26 14:11:07 -0300

    ADD Helper methods

--- IteratedWidth/parse_tree.py
+++ IteratedWidth/parse_tree.py
@@ -9,6 +9,7 @@ class Node:
         self.children = []
         self.is_terminal = is_terminal
         self.parent = parent
+        self.can_mutate = True
 
 class ParseTree:
     """ Parse Tree implementation given the self.dsl given. """
@@ -25,12 +26,13 @@ class ParseTree:
                         node_id = 0, 
                         value = self.dsl.start, 
                         is_terminal = False, 
-                        parent = ''
+                        parent = None
                         )
         self.max_nodes = max_nodes
         self.current_id = 0
         self.novelty = 0
         self.k = k
+        self.depth = 0
         self.is_IW = is_IW
         self.k_pairing = {}
         self.k_pairing_values = {}
@@ -39,12 +41,41 @@ class ParseTree:
             self.k_pairing_values[i] = []
         self.program = 'S'
 
+    '''
     def __eq__(self, other):
         """Overrides the default implementation. """
         
         if isinstance(other, ParseTree):
             return self.program == other.program
         return False
+    '''
+    
+    def set_fixed_nodes(self):
+        list_of_nodes = []
+        self._get_traversal_list_of_nodes(self.root, list_of_nodes)
+        for node in list_of_nodes:
+            #node.add_property('can_mutate', lambda self: False)
+            node.can_mutate = False
+
+    def __eq__(self, other):
+        """Overrides the default implementation. """
+        
+        if not isinstance(other, ParseTree):
+            return False
+        else:
+            return self.tree_equality(self.root, other.root)
+        return False
+    
+    def tree_equality(self, node_1, node_2):
+        if len(node_1.children) != len(node_2.children):
+            return False
+        elif node_1.value != node_2.value:
+            return False
+        else:
+            for i in range(len(node_1.children)):
+                if not self.tree_equality(node_1.children[i], node_2.children[i]):
+                    return False
+        return True
 
     def is_finished(self):
         """ Check if the current tree is complete (no non-terminal nodes). """
@@ -92,22 +123,49 @@ class ParseTree:
                             node_id = node_id, 
                             value = child, 
                             is_terminal = is_terminal, 
-                            parent = parent_node.value
+                            parent = parent_node
                             )
             parent_node.children.append(child_node)
 
     def update_k_pairing(self):
         self.k_pairing = {}
         self.k_pairing_values = {}
-        node_list = self.get_all_traversal_list_of_nodes()
+        all_paths = self.get_all_paths()
+
+        # Instantiate the lists for the k_pairings
         for i in range(1, self.k+1):
-            i_pairing = []
-            i_pairing_values = []
-            for j in range(len(node_list) - i + 1):
-                i_pairing.append([node_list[n] for n in range(j, j+i)])
-                i_pairing_values.append([node_list[n].value for n in range(j, j+i)])
-            self.k_pairing[i] = i_pairing
-            self.k_pairing_values[i] = i_pairing_values
+            self.k_pairing[i] = []
+            self.k_pairing_values[i] = []
+
+        # Special case - root (S)
+        if not self.root.children:
+            self.k_pairing[1].append([self.root])
+            self.k_pairing_values[1].append(['S'])
+            return
+        # Special case - 2 nodes (S - statement)
+        if len(self.root.children) == 2 and not self.root.children[1].children:
+            self.k_pairing[1].append([self.root])
+            self.k_pairing[1].append([self.root.children[1]])
+            self.k_pairing_values[1].append(['S'])
+            self.k_pairing_values[1].append(['statement'])
+            return
+        # Collect the k-pairs from each path
+        for path in all_paths:
+            for i in range(1, self.k+1):
+                i_pairing = []
+                i_pairing_values = []
+                for j in range(len(path) - i + 1):
+                    i_pairing.append([path[n] for n in range(j, j+i)])
+                    i_pairing_values.append([path[n].value for n in range(j, j+i)])
+                self.k_pairing[i] += i_pairing
+                self.k_pairing_values[i] += i_pairing_values
+
+        for key in self.k_pairing_values:
+            no_dupe_pairing = []
+            for elem in self.k_pairing_values[key]:
+                if elem not in no_dupe_pairing:
+                    no_dupe_pairing.append(elem)
+            self.k_pairing_values[key] = no_dupe_pairing
 
     def expand_specific_node(self, node, dsl_entry):
         """
@@ -123,7 +181,7 @@ class ParseTree:
                             node_id = node_id, 
                             value = child, 
                             is_terminal = is_terminal, 
-                            parent = node.value
+                            parent = node
                             )
             node.children.append(child_node)
         # Update k-pairing only if IW is running this tree
@@ -153,7 +211,7 @@ class ParseTree:
                             node_id = node_id, 
                             value = child, 
                             is_terminal = is_terminal, 
-                            parent = parent_node.value
+                            parent = parent_node
                             )
             parent_node.children.append(child_node)
 
@@ -182,16 +240,13 @@ class ParseTree:
         for child_node in start_node.children:
             self._finish_tree(child_node)
 
-    def _is_terminal(self, dsl_entry):
+    def _is_terminal(self, child):
         """ 
         Check if the DSL entry is a terminal one. That is, check if the entry
         has no way of expanding.
         """
-
-        tokens = self._tokenize_dsl_entry(dsl_entry)
-        for token in tokens:
-            if token in self.dsl._grammar:
-                return False
+        if child in self.dsl._grammar:
+            return False
         return True
 
     def _tokenize_dsl_entry(self, dsl_entry):
@@ -220,7 +275,37 @@ class ParseTree:
         # Transform '\n' and '\t' into actual new lines and tabs
         whole_program = codecs.decode(whole_program, 'unicode_escape')
 
-        return whole_program
+        # Manually swap the indentation tokens into actual indentations
+        # I(.) maintains the current indentation
+        # I(-) decreases by 1 the current indentation
+        # I(+) increases by 1 the current indentation
+        initial_indent = 3
+        indented_program = ''
+        for token in whole_program.split():
+            if token == 'I(.)':
+                indented_program += '\n' + '\t' * initial_indent
+            elif token == 'I(+)':
+                initial_indent += 1
+                indented_program += '\n' + '\t' * initial_indent
+            elif token == 'I(-)':
+                initial_indent -= 1
+                indented_program += '\n' + '\t' * initial_indent
+            else:
+                indented_program += token + ' '
+
+        indented_program = codecs.decode(indented_program, 'unicode_escape')
+
+        # Removes blank lines
+        final_program = ''
+        for line in indented_program.splitlines():
+            blank_line = True
+            for c in line:
+                if c.isalnum():
+                    blank_line = False
+                    break
+            if not blank_line:
+                final_program += line + '\n'
+        return final_program
 
     def _get_traversal_list_of_nodes(self, node, list_of_nodes):
         """
@@ -262,7 +347,8 @@ class ParseTree:
         """ Helper method for self.get_tree_leaves. """
 
         if not node.children:
-            leaves.append(node)
+            if node.value not in ['I(.)', 'I(+)', 'I(-)']:
+                leaves.append(node)
         for child in node.children:
             self._get_tree_leaves(child, leaves)
 
@@ -297,17 +383,50 @@ class ParseTree:
             print(
                 node.value, 
                 ', id = ', node.node_id, 
-                ', node parent = ', node.parent
+                ', node parent = None'
                 )
         else:
-            print(
-                indentation, 
-                node.value, 
-                ', id = ', node.node_id, 
-                ', node parent = ', node.parent
-                )
+            if node.value not in ['I(.)', 'I(+)', 'I(-)']:
+                print(
+                    indentation, 
+                    node.value, 
+                    ', id = ', node.node_id, 
+                    ', node parent = ', node.parent.value
+                    )
         for child in node.children:
             self._print_tree(child, indentation + '    ')
+
+    def print_tree_to_file(self, file, to_print):
+        """ Prints the tree in a simplistic manner. Used for debugging. """
+        self._print_tree_to_file(self.root, '  ', file, to_print)
+        with open(file, 'a') as f:
+            print(file=f)
+
+    def _print_tree_to_file(self, node, indentation, file, to_print):
+        """ Helper method for self.print_tree. """
+
+        #For root
+        if indentation == '  ':
+            with open(file, 'a') as f:
+                print(to_print, file=f)
+                print(
+                    node.value, 
+                    ', id = ', node.node_id, 
+                    ', node parent = None',
+                    file=f
+                    )
+        else:
+            with open(file, 'a') as f:
+                if node.value not in ['I(.)', 'I(+)', 'I(-)']:
+                    print(
+                        indentation, 
+                        node.value, 
+                        ', id = ', node.node_id, 
+                        ', node parent = ', node.parent.value,
+                        file=f
+                        )
+        for child in node.children:
+            self._print_tree_to_file(child, indentation + '    ', file, to_print)
 
     def _update_nodes_ids(self, node):
         """ Update all tree nodes' ids. Used after a tree mutation. """
@@ -317,6 +436,20 @@ class ParseTree:
         for child_node in node.children:
             self._update_nodes_ids(child_node)
 
+    
+    def _can_mutate(self, node):
+        if node.value in self.dsl.terminal_nodes:
+            return True
+        else:
+            return False
+    
+    '''
+    def _can_mutate(self, node):
+        if node.can_mutate:
+            return True
+        else:
+            return False
+    '''
     def mutate_tree(self):
         """ Mutate a single node of the tree. """
 
@@ -328,7 +461,8 @@ class ParseTree:
                 raise Exception('Node randomly selected does not exist.' + \
                     ' Index sampled = ' + str(index_node) + \
                     ' Tree is printed above.')
-            if not node_to_mutate.is_terminal:
+            #if not node_to_mutate.is_terminal:
+            if self._can_mutate(node_to_mutate):
                 break
 
         # delete its children and mutate it with new values
@@ -356,3 +490,81 @@ class ParseTree:
         """ Return a 'deepcopy' copy of self. """
 
         return pickle.loads(pickle.dumps(self, -1))
+
+    def get_all_paths(self):
+        """
+        Calculate all possible paths from each leaf to all other leaves,
+        traversing it from left to right. The number of paths is squared to the
+        number of leaves in the tree ( (n * (n-1)) / 2 , n being the number of
+        leaves).
+        Return all paths (a list of Node objects) given the parse tree.
+        """
+
+        all_leaves = self.get_tree_leaves()
+        #all_leaves = [leaf for leaf in all_leaves if leaf.value not in ['I(.)', 'I(+)', 'I(-)']]
+        all_paths = []
+        # Iterate through each leaf of the tree to get its paths to other leaves
+        for leaf in all_leaves:
+            initial_list = [leaf]
+            # While it can still go up in the tree to get all upward paths
+            while initial_list[-1].parent != None:
+                #print('entrei no while')
+                all_downward_paths = []
+                # Get all downward paths from the node at the end of the list
+                self._get_downward_paths(all_downward_paths, initial_list[-1].parent, [], initial_list[-1])
+                # Downward path can output many paths (if it is leveled)
+                for i in range(len(all_downward_paths)):
+                    all_downward_paths[i] = initial_list + all_downward_paths[i]
+                # Append to the 'global' all_paths list
+                for path in all_downward_paths:
+                    all_paths.append(path)
+                # Needed if there are upward nodes (next while iteration)
+                initial_list.append(initial_list[-1].parent)
+
+        # Remove  paths containing indentation
+        new_all_paths = []
+        for path in all_paths:
+            invalid = False
+            for node in path:
+                if node.value in ['I(.)', 'I(+)', 'I(-)']:
+                    invalid = True
+                    break
+            if not invalid:
+                new_all_paths.append(path)
+
+        return new_all_paths
+
+    def _get_downward_paths(self, all_downward_paths, node, path, node_called):
+        """ 
+        Get all paths from node passing through all its children ramifications.
+        If a path reaches a leaf, add path to all_downward_paths.
+        Otherwise, append the current node to path.
+        """
+
+        path.append(node)
+        # If it's a leaf, end current path by adding it to all_downward_paths
+        if not node.children:
+            all_downward_paths.append(path)
+        else:
+            # Get the index of node_called to avoid adding it again to path.
+            # try-catch needed because of recursive calls, node_called might not
+            # be in node.children anymore.
+            try:
+                index = node.children.index(node_called)
+            except:
+                index = -1
+            if index == len(node.children):
+                return
+            # Start from that position onwards
+            for i in range(index+1, len(node.children)):
+                if node.children[i] == node_called:
+                    continue
+                copy_path = pickle.loads(pickle.dumps(path, -1))
+                # Get subsequent paths from node's children
+                self._get_downward_paths(
+                                            all_downward_paths, 
+                                            node.children[i], 
+                                            copy_path, 
+                                            node_called
+                                        )
+
